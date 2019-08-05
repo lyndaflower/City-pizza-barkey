279926391533bc8f54a89fd8ca888ebb7e699597
lyndaflower <umurerwalynda@gmail.com>
2019-08-05 13:47:53 +0200

    add structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 function Detail(name, location) {
     this.name = name;
-    this.location = location
+    this.location = location;
 }
 $(document).ready(function(){
 $("form#select").send(function(event){
-    
+    var inputtedName = $("input#name").val();
+    var inputLocation = $("inputLocation").val();
+    var newDetail = $("inputName, inputLocation");
 })
-})
+if ((pepperoni == large) && (crust == deep dish){
+    alert();
+});
