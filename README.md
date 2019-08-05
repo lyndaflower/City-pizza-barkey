e7192e24b3d2c7471e1915c6b589c848a2c0b5ea
lyndaflower <umurerwalynda@gmail.com>
2019-08-05 20:05:48 +0200

    i dont know the erroe

---
+++ README.md
@@ -0,0 +1,2 @@
+# City-pizza-barkey
+
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Order(size, crust, topping){
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+}
+$(document).ready(function(){
+    $("")
+})
+  
