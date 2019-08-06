66d54900d2df443a131a8282216233aa9f36081f
lyndaflower <umurerwalynda@gmail.com>
2019-08-06 07:12:12 +0200

    add input for number of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-function Order(crust, size, topping, number){
+function Order(crust, size, topping){
     this.crust = crust;
     this.size = size;
     this.topping = topping;
-    this.number = number;
 }
 Order.prototype.myOrder = function(){
-    return "thanks, you have ordered "+ this.crust + this.topping + this.number ;
+    return "thanks, you have ordered "+ this.crust + " and " +this.topping ;
 }
+$(document).ready(function(){
+    $("#send").click(function(event){
+        event.preventDafault();
+        var crust = $("select#deep").val();
+        var size = $("select#lar").val();
+        var topping = $("select#onio").val();
+
+    })
+})
